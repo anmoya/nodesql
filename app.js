8085abfdd8fe658fb64be349c832a4ecda4d7dc6
Alfonso Nicolás Moya Chamorro <alfonsonicolasmoyachamorro@MacBook-Pro-de-Alfonso.local>
2018-09-05 23:35:00 -0300

    first commit (no sql, usa mlab)

---
+++ app.js
@@ -0,0 +1,50 @@
+const express   = require('express');
+const app       = express();
+var fs = require('fs');
+var pdf = require('html-pdf');
+var html = null;
+var options = { format: 'Letter', orientation: 'portrait' };
+var ejs = require('ejs');
+
+const mongoose  = require('mongoose');
+
+mongoose.connect('mongodb://sphoidldoh:riojaneiro2018@ds245512.mlab.com:45512/shiloh');
+var authorSchema = new mongoose.Schema({
+    authorName: String,
+    authorLastName : String,
+    authorCountry : String,
+    authorCity : String,
+    authorBio : String,
+    authorBornDate : { type: Date, default: Date.now }
+});
+
+var Author = mongoose.model('Author', authorSchema);
+
+app.get('/', (req, res) => {
+    Author.find({}, (err, allAuthors) => {
+        if (err) console.log(err);
+        else {
+            ejs.renderFile('./views/reporteautores/index.ejs', { data : allAuthors }, (err, result) => {
+                if (result) {
+                    html = result;
+                }
+                else {
+                    res.send('An error ocurred');
+                    console.log(err);
+                }
+            });
+
+            pdf.create(html, options).toBuffer( (err, buffer) => {
+                if (err) return res.send(err);
+                res.type('pdf');
+                res.end(buffer, 'binary');
+            });
+            //res.render('reporteautores/index.ejs', { data : allAuthors });
+        }
+    });
+    
+});
+
+app.listen(5001,'localhost', () => {
+    console.log('Servidor corriendo...');
+});
